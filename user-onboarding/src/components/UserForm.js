5a12183f56a87187b569a614163dcba344dd72ee
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-08-07 15:52:42 -0400

    step 4 included now

--- user-onboarding/src/components/UserForm.js
+++ user-onboarding/src/components/UserForm.js
@@ -1,63 +1,84 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
-function UserForm({ values, errors, touched, isSubmitting }) {
+const UserForm = ({ values, errors, touched, isSubmitting, status }) => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    // status sometimes comes through as undefined
+    if (status) {
+      setUsers([...users, status]);
+    }
+  }, [status]);
   return (
-    <Form className="form">
-      <div className='container'>
-        <div className="field">
-          <label htmlFor="name">Name:</label>
-          <Field type="text" name="name" id="name" className="field" />
-          {touched.name && errors.name && <p>{errors.name}</p>}
+    <>
+      <Form className="form">
+        <div className="container">
+          <div className="field">
+            <label htmlFor="name">Name:</label>
+            <Field type="text" name="name" id="name" className="field" />
+            {touched.name && errors.name && <p>{errors.name}</p>}
+          </div>
+          <div className="field">
+            <label htmlFor="email">Email:</label>
+            <Field type="email" name="email" id="email" className="field" />
+            {touched.email && errors.email && <p>{errors.email}</p>}
+          </div>
+          <div className="field">
+            <label htmlFor="password">Password: </label>
+            <Field
+              type="password"
+              name="password"
+              id="password"
+              className="field"
+            />
+            {touched.password && errors.password && <p>{errors.password}</p>}
+          </div>
+          <div className="field">
+            <label htmlFor="terms">Accept Terms of Conditions</label>
+            <Field
+              type="checkbox"
+              name="terms"
+              id="terms"
+              checked={values.terms}
+            />
+            {touched.terms && errors.terms && <p>{errors.terms}</p>}
+          </div>
+          <Field component="select" name="dropdown">
+            <option value="pencil">Pencil</option>
+            <option value="marker">Marker</option>
+            <option value="pen">Pen</option>
+          </Field>
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        <div className='current'>
+        <h1>Current Users</h1>
+      {values.user && values.users.map(user => console.log("each user", user))}
+      {users
+        ? users.map(user => (
+            <p key={user.id} className="users">
+             {user.name}
+            </p>
+          ))
+        : null}
         </div>
-        <div className="field">
-          <label htmlFor="email">Email:</label>
-          <Field type="email" name="email" id="email" className="field" />
-          {touched.email && errors.email && <p>{errors.email}</p>}
         </div>
-        <div className="field">
-          <label htmlFor="password">Password: </label>
-          <Field
-            type="password"
-            name="password"
-            id="password"
-            className="field"
-          />
-          {touched.password && errors.password && <p>{errors.password}</p>}
-        </div>
-        <div className="field">
-          <label htmlFor="terms">Accept Terms of Conditions</label>
-          <Field
-            type="checkbox"
-            name="terms"
-            id="terms"
-            checked={values.terms}
-          />
-          {touched.terms && errors.terms && <p>{errors.terms}</p>}
-        </div>
-        <Field component="select" name="dropdown">
-        <option value="pencil">Pencil</option>
-        <option value="marker">Marker</option>
-        <option value="pen">Pen</option>
-      </Field>
-        <button type="submit" disabled={isSubmitting}>
-          Submit
-        </button>
-      </div>
-    </Form>
+      </Form>
+    </>
   );
-}
+};
 
 const FormikUserForm = withFormik({
-  mapPropsToValues({ name, email, password, terms, dropdown }) {
+  mapPropsToValues({ name, email, password, terms, dropdown, users }) {
     return {
       name: name || "",
       email: email || "",
       password: password || "",
       terms: terms || false,
-      dropdown: dropdown || 'pencil'
+      dropdown: dropdown || "pencil",
+      users: ['Example User']
     };
   },
 
@@ -74,14 +95,14 @@ const FormikUserForm = withFormik({
     terms: Yup.boolean().oneOf([true], "Must Check Box to proceed")
   }),
 
-  handleSubmit(values, { resetForm, setErrors, setSubmitting }) {
+  handleSubmit(values, { resetForm, setErrors, setSubmitting, setStatus }) {
     if (values.email === "ghostrider@yahoo.com") {
       setErrors({ email: "Thats already in use try again" });
     } else {
       axios
         .post("https://reqres.in/api/users", values)
         .then(res => {
-          console.log(res.data);
+          setStatus(res.data);
           resetForm();
           setSubmitting(false);
           window.alert(`Name: ${res.data.name}, Email: ${res.data.email}`);
